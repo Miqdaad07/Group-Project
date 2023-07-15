b1e5edde18b8e017b6c3fcaa50a629b4cc668d40
Miqdaad07 <138245075+Miqdaad07@users.noreply.github.com>
2023-07-15 16:04:55 +0100

    another push

---
+++ app.js
@@ -0,0 +1,101 @@
+// alert('Welcome to JS class')
+// HOW TO CREATE A VARAIBLE
+// let
+// const
+
+// LET
+// All varaibles must hold value
+// values; String, number, undefined array function, boolean, null object
+
+// let variable name = value
+let name ="Toby"
+name = "Miqdaad"
+let age = 9
+age = 22
+let isDone = true
+isDone = false
+
+// showing things on the console by using console.log()
+// console.log(name)
+// console.log(age)
+// console.log(isDone)
+
+// showing things on the console by using alert()
+// alert(name)
+// alert(age)
+// alert(isDone)
+let mylocation = "mushin"
+mylocation = "yaba"
+// console.log(mylocation)
+
+//const
+const myApiKey = 'hctyeauhie'
+// console.log(myApiKey)
+
+
+//Joining strings
+
+const myName = 'james'
+const lastName = 'ola'
+// console.log(myName + ' and ' + lastName + ' are good guys ')
+
+const myAge = 12;
+const thisAge = 18;
+
+//operators in  JS
+
+//Arithmetic operators in js.. +, -, *, /
+const num1 = 24
+const num2 = 2
+
+// console.log(num1 + num2)
+// console.log(num1 * num2)
+// console.log(num1 - num2)
+// console.log(num1 / num2)
+
+//Comparison Operators in JS .. ===, <=, >=, <, >, !==, ==
+
+// console.log(2 < 1)
+// console.log(2 === 2)
+
+// Logical operators, &&, ||, !
+
+// console.log(2 === 2 || 3=== '3')
+
+//Typeof operator
+// const com = true
+// console.log(typeof com)
+
+//Assignment Operators =, +=, -=, *=, /=, %=, **=, ++, --
+
+// const n1 = 1
+// const n2 = 2
+// console.log(n1 + n2)
+
+// let nu1 = 1
+// nu1 += 1
+// console.log(nu1)
+
+
+//Storing values in Javascript
+
+//Array[];
+
+// const storeNames = ['Mayowa', 'Miqdaad', 'ola', 'bola', 'Dhamiy']
+// console.log(storeNames)
+
+// remove the last item in an array using a method pop()
+// storeNames.pop()
+// console.log(storeNames)
+
+// remove the first item in an array using a method shift()
+// storeNames.shift()
+// console.log(storeNames)
+
+// insert a new item at the end of an array using a method push()
+// storeNames.push("Dhamiy")
+// console.log(storeNames)
+
+// insert a new item at the start of an array using a method unshift()
+// storeNames.unshift("Dhamiy")
+// console.log(storeNames)
